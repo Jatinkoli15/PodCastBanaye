656f220956f2bdc46479fff104b651a2240c626c
Jatin <jatinkoli65@gmail.com>
2024-10-01 11:45:53 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      },
+      {
+        protocol : 'https',
+        hostname: 'elegant-newt-390.convex.cloud'
+      }
+    ]
+  }
+};
+
+export default nextConfig;
